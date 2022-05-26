fbf08b5881c24836558a2d266b64043a45962a57
Dzham22 <84075594+Dzham22@users.noreply.github.com>
2022-05-26 13:14:25 +0300

    Update HomeTask.java

--- src/main/java/LessonNine/HomeTask.java
+++ src/main/java/LessonNine/HomeTask.java
@@ -1,21 +1,22 @@
-//package LessonNine;
-
-/*import kotlin.random.URandomKt;
+package LessonNine;
 
 import javax.xml.crypto.Data;
-import java.util.*;
+import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
+
+
 
 public class HomeTask {
     public static void main(String[] args) {
-        List<Student> students = Data.getStudentsList(); // в данном случае класс Data содержит тестовые данные, у вас они будут свои
-        Course randomCourse = Data.randomCourse(); // ваша реализация подачи случайного курса, можно просто создать объект и его передавать
+        List<Student> students = Data.getStudentsList();
+        Course randomCourse = Data.randomCourse();
         System.out.println(getUnicsCourses(students));
         System.out.println(inquisitiveStudents(students));
         System.out.println(getStudentsFromCourse(students, randomCourse));
     }
 
-    // 1. Написать функцию, принимающую список Student и возвращающую список уникальных курсов, на которые подписаны студенты.
+
     public static List<Course> getUnicsCourses(List<Student> students) {
         return students.stream()
                 .flatMap(student -> student.getAllCourses().stream())
@@ -23,7 +24,7 @@ public class HomeTask {
                 .collect(Collectors.toList());
     }
 
-    // 2. Написать функцию, принимающую на вход список Student и возвращающую список из трех самых любознательных (любознательность определяется количеством курсов).
+
     public static List<Student> inquisitiveStudents(List<Student> students) {
         return students.stream()
                 .sorted((s1, s2) -> (s2.getAllCourses().size()) - (s1.getAllCourses().size()))
@@ -31,7 +32,7 @@ public class HomeTask {
                 .collect(Collectors.toList());
     }
 
-    // 3. Написать функцию, принимающую на вход список Student и экземпляр Course, возвращающую список студентов, которые посещают этот курс.
+
     public static List<Student> getStudentsFromCourse(List<Student> students, Course course) {
         System.out.println("Ищем студентов проходящих курс " + course);
         return students.stream()
@@ -39,21 +40,25 @@ public class HomeTask {
                 .collect(Collectors.toList());
     }
 
+    static class Data {
+
+        public static List<Student> getStudentsList() {
+            return null;
+        }
+
+
+        public static Course randomCourse() {
+            return null;
+        }
+    }
 
     interface Student {
         String getName();
 
-
-        List<Course> getAllCourses();
-
-
+    List<Course> getAllCourses();
     }
 
+   interface Course {
 
-
-
-
-    interface Course {
-    }
+   }
 }
-*/
